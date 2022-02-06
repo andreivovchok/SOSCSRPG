a623c4890c419b1d20c4baaba90362f3542cbef7
Andrei Vovchok <andreivovchok@bk.ru>
2022-02-07 00:25:22 +0300

    Добавил класс Location

--- SOSCSRPG/Engine/ViewModels/GameSession.cs
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -8,6 +8,8 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+
 
         public GameSession()
         {
@@ -18,6 +20,13 @@ namespace Engine.ViewModels
             CurrentPlayer.Gold = 10000000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Дом";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "Это твой дом";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
         }
     }
 }
