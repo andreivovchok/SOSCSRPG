149805e511cef10cf3db1351f67805cf09f76421
andreivovchok <87602003+andreivovchok@users.noreply.github.com>
2022-02-07 13:51:29 +0300

    Добавил две локации

--- SOSCSRPG/Engine/Factories/WorldFactory.cs
+++ SOSCSRPG/Engine/Factories/WorldFactory.cs
@@ -11,7 +11,17 @@ namespace Engine.Factories
         {
             World newWorld = new World();
 
-            newWorld.AddLocation(0, -1, "Дом", "Это твой дом", "/Engine;component/Images/Locations/Home.png");
+            newWorld.AddLocation(-2, -1, "Поле Фермера",
+                "Здесь растут ряды кукурузы, между которыми прячутся гигантские крысы",
+                "/Engine;component/Images/Locations/FarmFields.png");
+
+            newWorld.AddLocation(-1, 1, "Дом Фермера",
+                "Это дом твоего соседа, фермера Теда",
+                "/Engine;component/Images/Locations/Farmhouse.png");
+
+            newWorld.AddLocation(0, -1, "Дом",
+                "Это твой дом",
+                "/Engine;component/Images/Locations/Home.png");
 
             return newWorld;
         }
