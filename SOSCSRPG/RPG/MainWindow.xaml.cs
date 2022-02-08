cc07832e62a7fcf9f9e6049386a9ab07eec335d2
andreivovchok <87602003+andreivovchok@users.noreply.github.com>
2022-02-08 11:04:20 +0300

    Добавил кнопки перемещения

--- SOSCSRPG/RPG/MainWindow.xaml.cs
+++ SOSCSRPG/RPG/MainWindow.xaml.cs
@@ -17,11 +17,6 @@ namespace RPG
             _gameSession = new GameSession();
 
             DataContext = _gameSession;
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
-        }
+        }        
     }
 }
