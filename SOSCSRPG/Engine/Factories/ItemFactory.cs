44723f33a9ec59d42762ade4d3cccac07fe44682
Andrei Vovchok <andreivovchok@bk.ru>
2022-02-15 19:58:25 +0300

    Добавил GameItem в ItemFactory

--- SOSCSRPG/Engine/Factories/ItemFactory.cs
+++ SOSCSRPG/Engine/Factories/ItemFactory.cs
@@ -19,6 +19,11 @@ namespace Engine.Factories
             _standardGameItems.Add(new Weapon(1002, "Ржавый меч", 5, 1, 3));
             _standardGameItems.Add(new GameItem(9001, "Клык змеи", 1));
             _standardGameItems.Add(new GameItem(9002, "Змеиная кожа", 2));
+            _standardGameItems.Add(new GameItem(9003, "Крысиный хвост", 1));
+            _standardGameItems.Add(new GameItem(9004, "Крысиный мех", 2));
+            _standardGameItems.Add(new GameItem(9005, "Клык паука", 1));
+            _standardGameItems.Add(new GameItem(9006, "Шелк паука", 2));
+
 
 
         }
