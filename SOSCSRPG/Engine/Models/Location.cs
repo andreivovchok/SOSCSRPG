887ac46760ee7d997d0f2cf863ae76dc408f03c4
Andrei Vovchok <andreivovchok@bk.ru>
2022-02-15 21:18:35 +0300

    Создал метод AddMonster и GetMonster в классе Location

--- SOSCSRPG/Engine/Models/Location.cs
+++ SOSCSRPG/Engine/Models/Location.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
+using Engine.Factories;
 
 namespace Engine.Models
 {
@@ -12,5 +15,47 @@ namespace Engine.Models
         public string Description { get; set; }
         public string ImageName { get; set; }
         public List<Quest> QuestAvailableHere { get; set; } = new List<Quest>();
+
+        public List<MonsterEncounter> MonstersHere { get; set; } = 
+            new List<MonsterEncounter>();
+
+        public void AddMonster(int monsterID, int chanceOfEncoutering)
+        {
+            if (MonstersHere.Exists(m => m.MonsterID == monsterID))
+            {
+                MonstersHere.First(m => m.MonsterID == monsterID)
+                    .ChanceOfEncountering = chanceOfEncoutering;
+            }
+            else
+            {
+                MonstersHere.Add(new MonsterEncounter(monsterID,chanceOfEncoutering));
+            }
+        }
+
+        public Monster GetMonster()
+        {
+            if (!MonstersHere.Any())
+            {
+                return null;
+            }
+
+            int totalChances = MonstersHere.Sum(m => m.ChanceOfEncountering);
+
+            int randomNumber = RandomNumberGenerator.SimpleNumberBetween(1, totalChances);
+
+            int ranningTotal = 0;
+
+            foreach (MonsterEncounter monsterEncounter in MonstersHere)
+            {
+                ranningTotal += monsterEncounter.ChanceOfEncountering;
+
+                if (randomNumber <= ranningTotal)
+                {
+                    return MonsterFactory.GetMonster(monsterEncounter.MonsterID);
+                }
+            }
+
+            return MonsterFactory.GetMonster(MonstersHere.Last().MonsterID);
+        }
     }
 }
