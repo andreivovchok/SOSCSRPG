ac85596eea33ea70915b2376a9ba17e785d6e94e
Andrei Vovchok <andreivovchok@bk.ru>
2022-02-17 19:56:55 +0300

    Добавил два свойства MinDamage / MaxDamage  в класс Monster

--- SOSCSRPG/Engine/Models/Monster.cs
+++ SOSCSRPG/Engine/Models/Monster.cs
@@ -24,6 +24,8 @@ namespace Engine.Models
                 OnPropertyChanged(nameof(HitPoints));
             }
         }
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
 
         public int RewardExperiencePoints { get; private set; }
         public int RewardGold { get; private set; }
@@ -32,12 +34,15 @@ namespace Engine.Models
 
         public Monster(string name, string imageName,
             int maximumHitPoints, int hitPoints,
+            int minimumDamage, int maximumDamage,
             int rewardExperiencePoints, int rewardGold)
         {
             Name = name;
             ImageName = string.Format("/Engine;component/Images/Monster/{0}", imageName);
             MaximumHitPoints = maximumHitPoints;
             HitPoints = hitPoints;
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
 
